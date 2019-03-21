1951ae6570224d389d3d39034f22f1e284497e6d
Jordi Joan <jordi.joan@gmail.com>
2019-03-21 09:44:26 +0100

    Animation working for multiple species.

--- brains.js
+++ brains.js
@@ -2,24 +2,25 @@ $(function() { // Init jQuery
   
 // Show hide  
 $('.show').bind('click', function() {
-    if ( $( this ).parent().parent().parent().hasClass( 'off' ) ) {
+    var el = $( this ).parent().parent().parent();
+    if ( $(el).hasClass( 'off' ) ) {
         // ON
-        $( this ).parent().parent().parent().addClass('on');
-        $( this ).parent().parent().parent().removeClass('off');
+        $( el ).addClass('on');
+        $( el ).removeClass('off');
         $( this ).attr('src', 'img/icon-hide.svg');
         $( this ).prev().animate( {opacity: 1} );
-        $( this ).parent().parent().parent().removeClass('animalOn');
-        $( this ).parent().parent().parent().addClass('animalOff');
-        $( 'h1, h4, .desc, .species' ).slideDown('fast');
+        $( el ).removeClass('animalOn');
+        $( el ).addClass('animalOff');
+        $( el ).find('h1, h4, p.desc, p.species').slideDown('fast');
     } else {
         // OFF
-        $( this ).parent().parent().parent().addClass('off');
-        $( this ).parent().parent().parent().removeClass('on');
+        $( el ).addClass('off');
+        $( el ).removeClass('on');
         $( this ).attr('src', 'img/icon-show.svg');
         $( this ).prev().animate( {opacity: .1} );
-        $( this ).parent().parent().parent().removeClass('animalOff');
-        $( this ).parent().parent().parent().addClass('animalOn');
-        $( 'h1, h4, .desc, .species' ).slideUp('fast');
+        $( el ).removeClass('animalOff');
+        $( el ).addClass('animalOn');
+        $( el ).find('h1, h4, p.desc, p.species').slideUp('fast');
     }
 
     
