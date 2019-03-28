b407dd36c532a25832d3b52b900ce9737b9e90d4
Shaun Lee <shaun@stet.co.nz>
2019-03-28 16:04:58 +1300

    I need help with adding captions and the global show/hide btton.

--- brains.js
+++ brains.js
@@ -1,5 +1,7 @@
 $(function() { // Init jQuery
-  
+
+
+    
 // Show hide  
 $('.show').bind('click', function() {
     var el = $( this ).parent().parent().parent();
@@ -22,13 +24,48 @@ $('.show').bind('click', function() {
         $( el ).addClass('animalOn');
         $( el ).find('h1, h4, p.desc, p.species').slideUp('fast');
     }
-
-    
 });
     
+/*
+Show hide rollover thing that Jordi can write?
+If you do please delete this CSS class '.animal-pics .photo .show:hover'
+
+function toggleShowHide {
+    if xxx has class 'off' {
+        $( this ).attr('src', 'img/icon-hide.svg');
+    } else {
+        $( this ).attr('src', 'img/icon-hide.svg');
+    }
+}
+.show.blind mouseover {
+    toggleShowHide();
+}
+.show.blind mouseout {
+    toggleShowHide();
+}
+
+*/
+    
 // Build Galleries
-$( '.gallery' ).each(function() {
-    $( this ).find('a').simpleLightbox();
+$('.gallery').each(function() {
+    $( this ).find('a').simpleLightbox({
+        captions:true,
+        captionDelay:0,
+        captionSelector:'img',
+        captionType:'attr',
+        captionPosition:'bottom',
+        captionClass:'',
+        captionsData:'title' // None of these options working, trying to set up captions!
+    });
+});
+
+    
+// Show all button
+$('nav a:last-child').click(function() {
+  alert( "Jordi makes all the animals come alive :D" );
+});
+$('nav a:last-child').hover(function() {
+    $( this ).children('img').attr('src', 'img/_hide.png'); // And this mouseout?
 });
     
 }); // End jQuery
