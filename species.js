de3475657633c6abb6aaed8dbb1ae63ae5b5d9af
Jordi Joan <jordi.joan@gmail.com>
2019-04-02 08:54:10 +0100

    Species in a separate file. Tabs working.

---
+++ species.js
@@ -0,0 +1,152 @@
+var species = [
+    {
+        "category": "birds",
+        "name":"Oystercatcher",
+        "subname":"Torea, Haematopus",
+        "folder": "oystercatcher",
+        "photos":[
+            {"name":"a", "title":"Photo by Shaun Lee"},
+            {"name":"b", "title":"Photo by Emily Harris"},
+            {"name":"c", "title":"Photo by Shaun Lee"},
+            {"name":"d", "title":"Photo by Shaun Lee"}
+        ],
+        "desc":"5cm in length. Oystercatchers walk on their toes so heel not always visible. Toes more chunky than Spur-winged plover.",
+        "similar":[
+            {"link":"http://nzbirdsonline.org.nz/species/south-island-pied-oystercatcher","name":"South Island Pied Oystercatcher"},
+            {"link":"http://nzbirdsonline.org.nz/species/variable-oystercatcher","name":"Variable Oystercatcher"}
+        ]
+    },
+    {
+        "category": "birds",
+        "name":"Stilt",
+        "subname":"Himantopus",
+        "folder": "stilt",
+        "photos":[
+            {"name":"a", "title":"Photo by Shaun Lee"},
+            {"name":"b", "title":"Photo by Shaun Lee"},
+            {"name":"c", "title":"Photo by Shaun Lee"},
+            {"name":"d", "title":"Photo by Shaun Lee"}
+        ],
+        "desc":"4cm in length. Slimmer toes than oystercatcher and plover.",
+        "similar":[
+            {"link":"http://www.nzbirdsonline.org.nz/species/pied-stilt","name":"Pied Stilt"},
+            {"link":"http://nzbirdsonline.org.nz/species/black-stilt","name":"Black Stilt"}
+        ]
+    },
+    {
+        "category": "birds",
+        "name":"Dotterel",
+        "subname":"Tūturiwhatu, Charadrius Obscurus",
+        "folder": "dotterel",
+        "photos":[
+            {"name":"a", "title":"Photo by Emily Roberts"}
+        ],
+        
+        "desc":"3cm in length with asymmetric toes. Dotterels walk on their toes so heel not often visible.",
+        "similar":[
+            {"link":"http://nzbirdsonline.org.nz/species/new-zealand-dotterel","name":"New Zealand Dotterel"}
+        ]
+    },
+    {
+        "category": "birds",
+        "name":"Pied Shag",
+        "subname":"Kawau, Phalacrocorax varius",
+        "folder": "pied-shag",
+        "photos":[
+            {"name":"a", "title":"Photo by Eliane Lagnaz"},
+            {"name":"b", "title":"Photo by Eliane Lagnaz"},
+            {"name":"c", "title":"Photo by Eliane Lagnaz"}
+        ],
+        
+        "desc":"9cm in length. More weight on the outer toe.",
+        "similar":[
+            {"link":"http://nzbirdsonline.org.nz/species/pied-shag","name":"Pied Shag"}
+        ]
+    },
+    {
+        "category": "birds",
+        "name":"Pukeko",
+        "subname":"Australasian swamphen, Porphyrio melanotus",
+        "folder": "pukeko",
+        "photos":[
+            {"name":"a", "title":"Left front foot. Photo by Shaun Lee"}
+        ],
+        "desc":"? in length.",
+        "similar":[
+            {"link":"http://nzbirdsonline.org.nz/species/pukeko","name":"Pukeko"}
+        ]
+    },
+    {
+        "category": "mammals",
+        "name":"Hedgehog",
+        "subname":"Erinaceus europaeus occidentalis",
+        "folder": "hedgehog",
+        "photos":[
+            {"name":"a", "title":"Left front foot. Photo by Shaun Lee"},
+        ],
+        "desc":"Hedgehog toe pads are larger and closer to the central pad compared with rat or ferret prints. - http://www.landcareresearch.co.nz",
+        "similar":[
+            {"link":"http://www.pestdetective.org.nz/culprits/hedgehog/","name":"Hedgehog"}
+        ]
+    },
+    {
+        "category": "birds",
+        "name":"Southern black-backed gull",
+        "subname":"Karoro, Kelp gull, Larus dominicanus",
+        "folder": "southern-black-backed-gull",
+        "photos":[
+            {"name":"a", "title":"Photo by Shaun Lee"},
+            {"name":"b", "title":"Photo by Shaun Lee"},
+            {"name":"c", "title":"Photo by Shaun Lee"}
+        ],
+        "desc":"7cm in length with webbing on feet noticeable in soft sand.",
+        "similar":[
+            {"link":"http://www.nzbirdsonline.org.nz/species/southern-black-backed-gull","name":"Southern black-backed gull"}
+        ]
+    },
+    {
+        "category": "birds",
+        "name":"Gull",
+        "subname":"Larus, Red-billed gull or Black-billed gull, Tarāpunga or Tarāpuka",
+        "folder": "gull",
+        "photos":[
+            {"name":"a", "title":"Photo by Shaun Lee"},
+            {"name":"b", "title":"Photo by Shaun Lee"},
+            {"name":"c", "title":"Photo by Shaun Lee"},
+            {"name":"d", "title":"Photo by Shaun Lee"}
+        ],
+        "desc":"4.5cm in length with webbing visible in soft sand. Smaller prints than black-backed gull.",
+        "similar":[
+            {"link":"http://www.nzbirdsonline.org.nz/species/red-billed-gull","name":"Red-billed gull"},
+            {"link":"http://www.nzbirdsonline.org.nz/species/black-billed-gull","name":"Black-billed gull"}
+        ]
+    },
+    {
+        "category": "mammals",
+        "name":"Wild European Rabbit",
+        "subname":"Rāpeti, Oryctolagus cuniculus",
+        "folder": "rabbit",
+        "photos":[
+            {"name":"a", "title":"All four feet of a small rabbit landing together. Photo by Shaun Lee"},
+        ],
+        "desc":"Back feet much longer than front feet. Length of back feet not always obvious in sand if running.",
+        "similar":[
+            {"link":"http://www.pestdetective.org.nz/culprits/rabbit/","name":"Rabbit"}
+        ]
+    },
+    {
+        "category": "birds",
+        "name":"Skylark or Pipit",
+        "subname":"Alauda arvensis or Anthus novaeseelandiae",
+        "folder": "skylark",
+        "photos":[
+            {"name":"a", "title":"Photo by Shaun Lee"},
+            {"name":"b", "title":"Walking. Photo by Shaun Lee"}
+        ],
+        "desc":"3cm in length. Back claw drags when walking.",
+        "similar":[
+            {"link":"http://www.nzbirdsonline.org.nz/species/eurasian-skylark","name":"Eurasian Skylark"},
+            {"link":"http://www.nzbirdsonline.org.nz/species/new-zealand-pipit","name":"Pipit"}
+        ]
+    }
+];
