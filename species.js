4d7c123e2d9a7249ec7ceac699211aac014a682f
Shaun Lee <shaun@stet.co.nz>
2019-04-12 09:11:49 +1200

    Update species.js
    Taxomic order

--- species.js
+++ species.js
@@ -1,55 +1,37 @@
+// Oganised by taxonomy, ref Birds New Zealand feild guide
+
 var species = [
+    // Kiwi
     {
         "category": "birds",
-        "name":"Oystercatcher",
-        "subname":"Torea, Haematopus",
-        "folder": "oystercatcher",
+        "name":"Kiwi",
+        "subname":"Apterygidae, Apteryx",
+        "folder": "kiwi",
         "photos":[
-            {"name":"a", "title":"South Island Pied Oystercatcher, Photo by Shaun Lee"},
-            {"name":"b", "title":"South Island Pied Oystercatcher, Photo by Shaun Lee"},
-            {"name":"c", "title":"Variable Oystercatcher, Photo by Shaun Lee"},
-            {"name":"d", "title":"Variable Oystercatcher, Photo by Shaun Lee"}
+            {"name":"a", "title":"Photo by Jordi Tablada"},
+            {"name":"b", "title":"Photo by Jordi Tablada"},
+            {"name":"c", "title":"Photo by Jordi Tablada"},
         ],
-        "desc":"5cm in length. Oystercatchers walk on their toes so heel not always visible. Toes more chunky than Spur-winged plover.",
+        "desc":"11cm long, no fourth (hind) toe unless very deep mud. Often associated with probe holes.",
         "similar":[
-            {"link":"http://nzbirdsonline.org.nz/species/south-island-pied-oystercatcher","name":"South Island Pied Oystercatcher"},
-            {"link":"http://nzbirdsonline.org.nz/species/variable-oystercatcher","name":"Variable Oystercatcher"}
+            {"link":"http://nzbirdsonline.org.nz/species/north-island-brown-kiwi","name":"North Island brown kiwi"},
+            {"link":"http://nzbirdsonline.org.nz/species/okarito-brown-kiwi","name":"Okarito brown kiwi"},
+            {"link":"http://nzbirdsonline.org.nz/species/southern-brown-kiwi","name":"Southern brown kiwi"},
+            {"link":"http://nzbirdsonline.org.nz/species/little-spotted-kiwi","name":"Little spotted kiwi"},
+            {"link":"http://nzbirdsonline.org.nz/species/great-spotted-kiwi","name":"Great spotted kiwi"}
         ]
     },
-    {
-        "category": "birds",
-        "name":"Stilt",
-        "subname":"Himantopus",
-        "folder": "stilt",
-        "photos":[
-            {"name":"a", "title":"Photo by Shaun Lee"},
-            {"name":"b", "title":"Photo by Shaun Lee"},
-            {"name":"c", "title":"Photo by Shaun Lee"},
-            {"name":"d", "title":"Photo by Shaun Lee"}
-        ],
-        "desc":"4cm in length. Slimmer toes than oystercatcher and plover.",
-        "similar":[
-            {"link":"http://www.nzbirdsonline.org.nz/species/pied-stilt","name":"Pied Stilt"},
-            {"link":"http://nzbirdsonline.org.nz/species/black-stilt","name":"Black Stilt"}
-        ]
-    },
-    {
-        "category": "birds",
-        "name":"Dotterel",
-        "subname":"Tūturiwhatu, Charadrius Obscurus",
-        "folder": "dotterel",
-        "photos":[
-            {"name":"a", "title":"Photo by Emily Roberts"},
-            {"name":"b", "title":"Photo by Shaun Lee"},
-            {"name":"c", "title":"Photo by Shaun Lee"},
-            {"name":"d", "title":"Photo by Shaun Lee"}
-        ],
-        
-        "desc":"3cm in length with asymmetric toes. Dotterels walk on their toes so heel not often visible.",
-        "similar":[
-            {"link":"http://nzbirdsonline.org.nz/species/new-zealand-dotterel","name":"New Zealand Dotterel"}
-        ]
-    },
+    // Gamebirds
+    // Waterfowl
+    // Grebes
+    // Penguins
+    // Albatrossses and Mollymawks
+    // Fullmars, Prions, Petrels and shearwaters
+    // Storm ptrels
+    // Tropicbirds
+    // Pelicans
+    // Gannets and boobies
+    // Shags
     {
         "category": "birds",
         "name":"Pied Shag",
@@ -66,7 +48,13 @@ var species = [
             {"link":"http://nzbirdsonline.org.nz/species/pied-shag","name":"Pied Shag"}
         ]
     },
-    {
+    // Darters
+    // Frigatebirds
+    // Herons, Egrets and Bitterns
+    // Ibises and Spoonbills
+    // Raptors
+    // Rails, Gallinules and Coots
+   {
         "category": "birds",
         "name":"Pukeko",
         "subname":"Australasian swamphen, Porphyrio melanotus",
@@ -113,19 +101,64 @@ var species = [
             {"link":"http://nzbirdsonline.org.nz/species/banded-rail","name":"Banded rail"}
         ]
     },
+    // Snipes, Sandpipers, Godwits and allies
+    // Oystercatchers
     {
-        "category": "mammals",
-        "name":"Hedgehog",
-        "subname":"Erinaceus europaeus occidentalis",
-        "folder": "hedgehog",
+        "category": "birds",
+        "name":"Oystercatcher",
+        "subname":"Torea, Haematopus",
+        "folder": "oystercatcher",
         "photos":[
-            {"name":"a", "title":"Left front foot. Photo by Shaun Lee"},
+            {"name":"a", "title":"South Island Pied Oystercatcher, Photo by Shaun Lee"},
+            {"name":"b", "title":"South Island Pied Oystercatcher, Photo by Shaun Lee"},
+            {"name":"c", "title":"Variable Oystercatcher, Photo by Shaun Lee"},
+            {"name":"d", "title":"Variable Oystercatcher, Photo by Shaun Lee"}
         ],
-        "desc":"Hedgehog toe pads are larger and closer to the central pad compared with rat or ferret prints. - http://www.landcareresearch.co.nz",
+        "desc":"5cm in length. Oystercatchers walk on their toes so heel not always visible. Toes more chunky than Spur-winged plover.",
         "similar":[
-            {"link":"http://www.pestdetective.org.nz/culprits/hedgehog/","name":"Hedgehog"}
+            {"link":"http://nzbirdsonline.org.nz/species/south-island-pied-oystercatcher","name":"South Island Pied Oystercatcher"},
+            {"link":"http://nzbirdsonline.org.nz/species/variable-oystercatcher","name":"Variable Oystercatcher"}
         ]
     },
+    // Stilts and Avocets
+    {
+        "category": "birds",
+        "name":"Stilt",
+        "subname":"Himantopus",
+        "folder": "stilt",
+        "photos":[
+            {"name":"a", "title":"Photo by Shaun Lee"},
+            {"name":"b", "title":"Photo by Shaun Lee"},
+            {"name":"c", "title":"Photo by Shaun Lee"},
+            {"name":"d", "title":"Photo by Shaun Lee"}
+        ],
+        "desc":"4cm in length. Slimmer toes than oystercatcher and plover.",
+        "similar":[
+            {"link":"http://www.nzbirdsonline.org.nz/species/pied-stilt","name":"Pied Stilt"},
+            {"link":"http://nzbirdsonline.org.nz/species/black-stilt","name":"Black Stilt"}
+        ]
+    },
+    // Plovers, Dotterels and Lapwings
+    {
+        "category": "birds",
+        "name":"Dotterel",
+        "subname":"Tūturiwhatu, Charadrius Obscurus",
+        "folder": "dotterel",
+        "photos":[
+            {"name":"a", "title":"Photo by Emily Roberts"},
+            {"name":"b", "title":"Photo by Shaun Lee"},
+            {"name":"c", "title":"Photo by Shaun Lee"},
+            {"name":"d", "title":"Photo by Shaun Lee"}
+        ],
+        
+        "desc":"3cm in length with asymmetric toes. Dotterels walk on their toes so heel not often visible.",
+        "similar":[
+            {"link":"http://nzbirdsonline.org.nz/species/new-zealand-dotterel","name":"New Zealand Dotterel"}
+        ]
+    },
+    // Pratincoles and Coursers
+    // Skuas
+    // Gulls
     {
         "category": "birds",
         "name":"Southern black-backed gull",
@@ -159,6 +192,7 @@ var species = [
             {"link":"http://www.nzbirdsonline.org.nz/species/black-billed-gull","name":"Black-billed gull"}
         ]
     },
+    // Noddies and Terns
     {
         "category": "birds",
         "name":"Caspian Tern",
@@ -175,20 +209,28 @@ var species = [
             {"link":"http://nzbirdsonline.org.nz/species/caspian-tern","name":"Caspian Tern"}
         ]
     },
-    {
-        "category": "mammals",
-        "name":"Wild European Rabbit",
-        "subname":"Rāpeti, Oryctolagus cuniculus",
-        "folder": "rabbit",
-        "photos":[
-            {"name":"a", "title":"All four feet of a small rabbit landing together. Photo by Shaun Lee"},
-            {"name":"b", "title":"All four feet of a small rabbit landing together, running fast. Photo by Shaun Lee"},
-        ],
-        "desc":"Back feet much longer than front feet. Length of back feet not always obvious in sand if running.",
-        "similar":[
-            {"link":"http://www.pestdetective.org.nz/culprits/rabbit/","name":"Rabbit"}
-        ]
-    },
+    // Pidgeons and doves
+    // Cockatoos, Parrots and parakeets
+    // Cuckoos
+    // Owls
+    // Swifts
+    // Kingfishers
+    // Rollers
+    // Wrens
+    // Wattlebirds
+    // Stitchbird
+    // Piopio
+    // Warblers
+    // Honeyeaters
+    // Cuckoo-shrikes and Trillers
+    // Bulbuls
+    // Whisters and allies
+    // Woodswallows
+    // Fantails
+    // Crows and Jays
+    // Monarch flycatcher
+    // Robins
+    // Larks
     {
         "category": "birds",
         "name":"Skylark or Pipit",
@@ -204,6 +246,48 @@ var species = [
             {"link":"http://www.nzbirdsonline.org.nz/species/new-zealand-pipit","name":"Pipit"}
         ]
     },
+    // Reed-warblers
+    // Grassbirds and allies
+    // White-eyes
+    // Swallows and Martins
+    // Thrushes
+    // Starlings and Mynas
+    // Old world sparrows
+    // Pipits and Wagtails
+    // Accentors
+    // Finches
+    // Buntings and new world sparrows
+    
+    // Mammals
+    {
+        "category": "mammals",
+        "name":"Wild European Rabbit",
+        "subname":"Rāpeti, Oryctolagus cuniculus",
+        "folder": "rabbit",
+        "photos":[
+            {"name":"a", "title":"All four feet of a small rabbit landing together. Photo by Shaun Lee"},
+            {"name":"b", "title":"All four feet of a small rabbit landing together, running fast. Photo by Shaun Lee"},
+        ],
+        "desc":"Back feet much longer than front feet. Length of back feet not always obvious in sand if running.",
+        "similar":[
+            {"link":"http://www.pestdetective.org.nz/culprits/rabbit/","name":"Rabbit"}
+        ]
+    },
+    {
+        "category": "mammals",
+        "name":"Hedgehog",
+        "subname":"Erinaceus europaeus occidentalis",
+        "folder": "hedgehog",
+        "photos":[
+            {"name":"a", "title":"Left front foot. Photo by Shaun Lee"},
+        ],
+        "desc":"Hedgehog toe pads are larger and closer to the central pad compared with rat or ferret prints. - http://www.landcareresearch.co.nz",
+        "similar":[
+            {"link":"http://www.pestdetective.org.nz/culprits/hedgehog/","name":"Hedgehog"}
+        ]
+    },
+    
+    // Other
     {
         "category": "other",
         "name":"Large Sand Scarab",
@@ -245,24 +329,6 @@ var species = [
             {"link":"https://www.reptiles.org.nz/herpetofauna/native/oligososma-aeneum","name":"Copper skink"},
             {"link":"https://www.reptiles.org.nz/herpetofauna/non-native/lampropholis-delicata","name":"Plague skink"}
         ]
-    },
-    {
-        "category": "birds",
-        "name":"Kiwi",
-        "subname":"Apterygidae, Apteryx",
-        "folder": "kiwi",
-        "photos":[
-            {"name":"a", "title":"Photo by Jordi Tablada"},
-            {"name":"b", "title":"Photo by Jordi Tablada"},
-            {"name":"c", "title":"Photo by Jordi Tablada"},
-        ],
-        "desc":"11cm long, no fourth (hind) toe unless very deep mud. Often associated with probe holes.",
-        "similar":[
-            {"link":"http://nzbirdsonline.org.nz/species/north-island-brown-kiwi","name":"North Island brown kiwi"},
-            {"link":"http://nzbirdsonline.org.nz/species/okarito-brown-kiwi","name":"Okarito brown kiwi"},
-            {"link":"http://nzbirdsonline.org.nz/species/southern-brown-kiwi","name":"Southern brown kiwi"},
-            {"link":"http://nzbirdsonline.org.nz/species/little-spotted-kiwi","name":"Little spotted kiwi"},
-            {"link":"http://nzbirdsonline.org.nz/species/great-spotted-kiwi","name":"Great spotted kiwi"}
-        ]
-    }
+    } // no comma on last item
+
 ];
